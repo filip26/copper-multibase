31ffeb1d68e1bd88a6bde97913c01fd853f46e13
Filip <filip26@gmail.com>
2023-11-21 00:40:55 +0100

    Refactor

--- src/main/java/com/apicatalog/multibase/Multibase.java
+++ src/main/java/com/apicatalog/multibase/Multibase.java
@@ -1,31 +1,58 @@
 package com.apicatalog.multibase;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import com.apicatalog.base.Base58;
 
+/**
+ * Represents multibase encoding.
+ */
 public class Multibase {
 
-    public static final Base BASE_58_BTC = new Base('z', 58, Base58::decode, Base58::encode);
+    public static final Multibase BASE_58_BTC = new Multibase('z', 58, Base58::decode, Base58::encode);
 
-    protected final Map<Character, Base> bases;
+    protected final char prefix;
+    protected final int length;
 
-    protected Multibase(Map<Character, Base> bases) {
-        this.bases = bases;
+    protected final Function<String, byte[]> decode;
+    protected final Function<byte[], String> encode;
+
+    public Multibase(
+            char prefix,
+            int length,
+            Function<String, byte[]> decode,
+            Function<byte[], String> encode) {
+        this.prefix = prefix;
+        this.length = length;
+        this.decode = decode;
+        this.encode = encode;
     }
 
-    public static Multibase getInstance(Base... bases) {
-        return new Multibase(
-                Arrays.stream(bases)
-                        .collect(Collectors.toMap(Base::prefix, Function.identity())));
+    /**
+     * A unique prefix identifying base encoding in encoded value.
+     * 
+     * @return the base encoding unique prefix
+     */
+    public char prefix() {
+        return prefix;
     }
 
-    public Optional<Base> getBase(final String encoded) {
+    /**
+     * An encoding length. e.g. 32, 58, 64.
+     * 
+     * @return the encoding length
+     */
+    public int length() {
+        return length;
+    }
 
+    /**
+     * Decodes the given data into byte array.
+     * 
+     * @param encoded to decodes
+     * @return encoded data as byte array
+     */
+    public byte[] decode(final String encoded) {
         if (encoded == null) {
             throw new IllegalArgumentException("The encoded value must not be null.");
         }
@@ -34,12 +61,33 @@ public class Multibase {
             throw new IllegalArgumentException("The encoded value be non empty string.");
         }
 
-        return Optional.ofNullable(bases.get(encoded.charAt(0)));
+        final char p = encoded.charAt(0);
+
+        if (prefix != p) {
+            throw new IllegalArgumentException("Unsupported multibase encoding [" + p + "], this instance process only [" + prefix + "].");
+        }
+
+        final String data = encoded.substring(0);
+
+        return decode.apply(data);
     }
 
-    public byte[] decode(String encoded) {
-        return getBase(encoded)
-                .map(base -> base.decode(encoded))
-                .orElseThrow(() -> new IllegalArgumentException("Unsupported multibase encoding [" + encoded.charAt(0) + "]."));
+    /**
+     * Encodes the given data into base encoded string.
+     * 
+     * @param data to encode
+     * @return a string representing the encoded data
+     */
+    public String encode(byte[] data) {
+
+        if (data == null) {
+            throw new IllegalArgumentException("The data must not be null.");
+        }
+
+        if (data.length == 0) {
+            throw new IllegalArgumentException("The data must be non empty byte array.");
+        }
+
+        return prefix + encode.apply(data);
     }
 }
