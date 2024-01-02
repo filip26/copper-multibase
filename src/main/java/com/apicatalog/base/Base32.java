f93f383d50ac48098873d8e3bdfc7a7d9f76f313
Filip <filip26@gmail.com>
2024-01-02 22:14:24 +0100

    Fix Base32

--- src/main/java/com/apicatalog/base/Base32.java
+++ src/main/java/com/apicatalog/base/Base32.java
@@ -11,12 +11,16 @@ public class Base32 {
             'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '2',
             '3', '4', '5', '6', '7',
     };
-    
+
     static int[] PADDING = new int[] {
-        6, 4, 3, 1, 0,
+            6, 4, 3, 1, 0,
     };
 
-    public static String encode(byte[] data) {
+    static int[] PADDING_REVERSE = new int[] {
+            0, 5 - 4, -1, 5 - 3, 5 - 2, -1, 5 - 1, -1,
+    };
+
+    public static String encode(final byte[] data) {
         if (data == null) {
             throw new IllegalArgumentException();
         }
@@ -24,26 +28,24 @@ public class Base32 {
             return "";
         }
 
-        final StringBuilder encoded = new StringBuilder();
+        final StringBuilder encoded = new StringBuilder(((data.length / 5) + (data.length % 5 > 0 ? 1 : 0)) * 8);
 
         int rest = -1;
-        
+
         for (int index = 0; index < data.length; index++) {
 
-            int shift = index % 5;
-
-            switch (shift) {
+            switch (index % 5) {
             case 0:
                 encoded.append(ALPHABET[data[index] >>> 3]);
-                rest = (0x07 & data[index]) << 2; 
+                rest = (0x07 & data[index]) << 2;
                 break;
-                
+
             case 1:
                 encoded.append(ALPHABET[rest | (data[index] >>> 6)]);
                 encoded.append(ALPHABET[(0x3f & data[index]) >>> 1]);
                 rest = (0x01 & data[index]) << 4;
                 break;
-                
+
             case 2:
                 encoded.append(ALPHABET[rest | (data[index] >>> 4)]);
                 rest = (0x0f & data[index]) << 1;
@@ -75,9 +77,110 @@ public class Base32 {
         return encoded.toString();
     }
 
-    public static byte[] decode(String encoded) {
-        // TODO Auto-generated method stub
-        return null;
+    public static byte[] decode(final String encoded) {
+        if (encoded == null) {
+            throw new IllegalArgumentException();
+        }
+        if (encoded.isEmpty()) {
+            return new byte[0];
+        }
+
+        char[] chars = encoded.toCharArray();
+        int decoded = 0;
+
+        int padding = getPaddingLength(chars);
+
+        final byte[] data = new byte[getDecodedLength(chars, padding)];
+
+        int rest = 0;
+
+        for (int index = 0; index < (chars.length - padding); index++) {
+
+            int code = charToCode(chars[index]);
+
+            switch (index % 8) {
+            case 0:
+                data[decoded] = (byte) (code << 3);
+                break;
+
+            case 1:
+                data[decoded] |= (byte) (0x07 & (code >>> 2));
+                rest = (0x03 & code) << 6;
+                decoded++;
+                break;
+
+            case 2:
+                data[decoded] = (byte) (rest | code << 1);
+                break;
+
+            case 3:
+                data[decoded] |= (byte) (0x01 & (code >>> 4));
+                rest = (0x0f & code) << 4;
+                decoded++;
+                break;
+
+            case 4:
+                data[decoded] = (byte) (rest | code >>> 1);
+                rest = (0x01 & code) << 7;
+                decoded++;
+                break;
+
+            case 5:
+                data[decoded] = (byte) (rest | code << 2);
+                break;
+
+            case 6:
+                data[decoded] |= (byte) (0x03 & (code >>> 3));
+                rest = (0x07 & code) << 5;
+                decoded++;
+                break;
+
+            case 7:
+                data[decoded] = (byte) (rest | code);
+                decoded++;
+                break;
+            }
+        }
+
+        return data;
     }
 
+    static final int getDecodedLength(final char[] data, int pads) {
+
+        final int total = (data.length / 8) * 5;
+
+        final int diff = PADDING_REVERSE[pads];
+
+        if (diff == -1) {
+            throw new IllegalArgumentException("Unknown pad size '" + pads + "'");
+        }
+
+        return total - diff;
+    }
+
+    static final int getPaddingLength(final char[] data) {
+
+        int pads = 0;
+
+        for (int index = 1; index < 8; index++) {
+            if (data[data.length - index] != '=') {
+                break;
+            }
+            pads++;
+        }
+
+        return pads;
+    }
+
+    static int charToCode(char ch) {
+//        System.out.println("CH " + ch);
+        if (ch >= 'A' && ch <= 'Z') {
+            return ch - (int) 'A';
+        }
+        if (ch >= '2' && ch <= '7') {
+            return ch - (int) '2' + 26;
+        }
+
+        throw new IllegalArgumentException();
+    }
 }
--- src/test/java/com/apicatalog/base/Base32Test.java
+++ src/test/java/com/apicatalog/base/Base32Test.java
@@ -27,13 +27,19 @@ public class Base32Test {
 
     static Stream<Arguments> testData() {
         return Stream.of(
-          Arguments.of("", "".getBytes()),
-          Arguments.of("MY======", "f".getBytes()),
-          Arguments.of("MZXQ====", "fo".getBytes()),
-          Arguments.of("MZXW6===", "foo".getBytes()),
-          Arguments.of("MZXW6YQ=", "foob".getBytes()),
-          Arguments.of("MZXW6YTB", "fooba".getBytes()),
-          Arguments.of("MZXW6YTBOI======", "foobar".getBytes())
-        );
+                Arguments.of("", "".getBytes()),
+                Arguments.of("AA======", new byte[] { 0 }),
+                Arguments.of("AE======", new byte[] { 1 }),
+                Arguments.of("MY======", "f".getBytes()),
+                Arguments.of("MZXQ====", "fo".getBytes()),
+                Arguments.of("MZXW6===", "foo".getBytes()),
+                Arguments.of("MZXW6YQ=", "foob".getBytes()),
+                Arguments.of("MZXW6YTB", "fooba".getBytes()),
+                Arguments.of("MZXW6YTBOI======", "foobar".getBytes()),
+                Arguments.of(
+                        "KRUGS4ZAMRXWG5LNMVXHIIDEMVZWG4TJMJSXGIDUNBSSAY3PNVWW63TMPEQHK43FMQQGEYLTMUQDMNBMEBRGC43FEAZTELBAMFXGIIDCMFZWKIBRGYQGK3TDN5SGS3THEBZWG2DFNVSXGLQKJF2CAYLMONXSAZDJONRXK43TMVZSA5DIMUQHK43FEBXWMIDMNFXGKLLGMVSWI4ZANFXCAZLOMNXWIZLEEBSGC5DBFQQHK43FEBXWMIDQMFSGI2LOM4QGS3RAMVXGG33EMVSCAZDBORQSYIDVONSSA33GEBXG63RNMFWHA2DBMJSXIIDDNBQXEYLDORSXE4ZANFXCAZLOMNXWIZLEEBSGC5DBFQQHK43FEBXWMIDENFTGMZLSMVXHIIDFNZRW6ZDJNZTSAYLMOBUGCYTFORZSYIDBNZSCAY3BNZXW42LDMFWCAZLOMNXWI2LOM5ZS4===",
+                        ("This document describes the commonly used base 64, base 32, and base 16 encoding schemes.\n"
+                        + "It also discusses the use of line-feeds in encoded data, use of padding in encoded data, use of non-alphabet characters in encoded data, use of different encoding alphabets, and canonical encodings.")                        
+                        .getBytes()));
     }
 }
